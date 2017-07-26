cff2704d7b067865b180c3f0a8c2971eb2b242fb
yuskam <ybkamaleri@gmail.com>
2017-07-26 15:10:17 +0200

    update med DT

--- doc/functions.R
+++ doc/functions.R
@@ -2,3 +2,6 @@
 omkode <- function(DT, ord, var, new, tall) {
   DT[grepl(ord, get(var)), paste0(new) := tall]
 }
+
+## Whitespace
+for (j in names(data)) set(data, j = j, value = data[[trimws(j)]])
